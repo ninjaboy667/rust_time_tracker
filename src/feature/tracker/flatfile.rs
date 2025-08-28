254cbfffdebec6ed64c28ee7d6aa90b7c6320d3e
Eli Barber <elibarber@hotmail.com>
2025-08-27 23:38:56 -0500

    add save database

--- src/feature/tracker/flatfile.rs
+++ src/feature/tracker/flatfile.rs
@@ -5,6 +5,8 @@
 // - lock  "lock file" tracker is running
 // datbase file: JSON doc
 
+use std::io::{Read, Write};
+use std::path::Path;
 use std::{fs::OpenOptions, path::PathBuf};
 
 use error_stack::ResultExt;
@@ -12,7 +14,7 @@ use error_stack::Result;
 use serde::Deserialize;
 use serde::Serialize;
 
-use crate::feature::tracker::StartTime;
+use crate::feature::tracker::{EndTime, StartTime};
 use crate::feature::tracker::TimeRecord;
 
 
@@ -28,6 +30,17 @@ pub struct FlatFileTracker {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct LockfileData {
     start_time: StartTime
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub struct FlatFileDatabase {
+    records: Vec<TimeRecord>
+}
+
+impl FlatFileDatabase {
+    pub fn push(&mut self, value: TimeRecord) {
+        self.records.push(value)
+    }
 }
 
 impl FlatFileTracker {
@@ -48,7 +61,7 @@ impl FlatFileTracker {
                     start_time
                 };
                 serde_json::to_string(&data).change_context(FlatFileTrackerError)
-                .attach_printable("failed to attach lockfile data")
+                .attach_printable("failed to attach lockfile data")?
             };
             
 
@@ -58,6 +71,9 @@ impl FlatFileTracker {
             .write(true)
             .create_new(true)
             .open(&self.lockfile)
+            .change_context(FlatFileTrackerError)
+            .attach_printable("unable to create new lockfile when starting tracker")?
+            .write_all(lockfile_data.as_bytes())
             .change_context(FlatFileTrackerError)
             .attach_printable("unable to create new lockfile when starting tracker")?;
         
@@ -73,8 +89,20 @@ impl FlatFileTracker {
         
         fn stop(&self) -> Result<(), FlatFileTrackerError> {
             //read time from the lockfile
-            //get end time
+            let start = read_lockfile(self.lockfile)?.start_time;
+            //get end 
+            
             // 3. create record to the database file
+            let end = EndTime::now();
+
+            let record = TimeRecord { start, end };
+
+            let mut db = load_database(&self.db)?;
+            db.push(record);
+            save_database(&self.db, &db)?;
+
+
+
             std::fs::remove_file(&self.lockfile)
                 .change_context(FlatFileTrackerError)
                 .attach_printable("unable to remove lockfile when stopping tracker")
@@ -84,6 +112,69 @@ impl FlatFileTracker {
             // load records and rturn an iterator
             Ok(vec![].into_iter())
         }
+}
+
+fn save_database<P>(path: P, db: &FlatFileDatabase) -> Result<(), FlatFileTrackerError> 
+where 
+    P: AsRef<Path> {
+        let db = serde_json::to_string(&db)
+        .change_context(FlatFileTrackerError)
+        .attach_printable("failed to serialize database")?;
+
+    OpenOptions::new()
+    .write(true)
+    .create(true)
+    .truncate(false)
+    .open(path.as_ref())
+    .change_context(FlatFileTrackerError)
+    .attach_printable("failed to open database")?
+    .write_all(&db.as_bytes())
+    .change_context(FlatFileTrackerError)
+    .attach_printable("failed to write database")?;
+Ok(())
+    
+}
+
+fn load_database<P>(db: P) -> Result<FlatFileDatabase, FlatFileTrackerError>
+where P: AsRef<Path> {
+
+    let mut db_buf = String::default();
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(false)
+        .open(db.as_ref())
+        .change_context(FlatFileTrackerError)
+        .attach_printable("failed to open database")?
+        .read_to_string(&mut db_buf)
+        .change_context(FlatFileTrackerError)
+        .attach_printable("failed to read database")?;
+
+    if db_buf.is_empty( ) {
+        Ok(FlatFileDatabase::default())
+    }
+     else {
+        Ok(serde_json::from_str(&db_buf)
+            .change_context(FlatFileTrackerError)
+            .attach_printable("failed to deserialize database")?)
+     }
+
+    
+}
+
+fn read_lockfile<P>(lockfile: P) -> Result<LockfileData, FlatFileTrackerError>
+where P: AsRef<Path>
+{
+    let file = OpenOptions::new()
+    .read(true)
+    .open(lockfile.as_ref())
+    .change_context(FlatFileTrackerError)
+    .attach_printable("failed to open lockfile")?;
+
+    Ok(serde_json::from_reader(file)
+.change_context(FlatFileTrackerError)
+.attach_printable("failed to deserialize lockfile")?)
 }
 
 
